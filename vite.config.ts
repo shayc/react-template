7b6ad6fa7f7ad63efef69712066442792596ea8a
Shay Cojocaru <shayc@outlook.com>
2024-08-22 00:57:02 +0300

    chore: add react testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    // ...
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
   },
 });
