70681e119cbb6facf4842f7326d323a81d5a2445
Shay Cojocaru <shayc@outlook.com>
2025-02-15 23:47:03 +0200

    feat(i18n): add English and Hebrew translations; refactor i18next initialization

--- src/tests/testUtils.tsx
+++ src/tests/testUtils.tsx
@@ -1,14 +1,15 @@
 import { render } from "@testing-library/react";
 import React, { ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./i18nForTests";
-
-const AllProviders = ({ children }: { children: ReactNode }) => (
-  <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
-);
+import { i18n } from "../i18n/i18n";
 
 const customRender = (ui: React.ReactElement, options = {}) =>
-  render(ui, { wrapper: AllProviders, ...options });
+  render(ui, {
+    wrapper: ({ children }: { children: ReactNode }) => (
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+    ),
+    ...options,
+  });
 
 export * from "@testing-library/react";
 export { customRender as render };
