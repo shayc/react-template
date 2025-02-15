38bcf077746f63cc045bfc23dfe037da92000983
Shay Cojocaru <shayc@outlook.com>
2025-02-15 20:31:04 +0200

    fix: add hebrew translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  function changeLanguage(lang: string) {
+    i18n.changeLanguage(lang).catch((error) => {
+      console.error("Failed to change language", error);
+    });
+  }
 
   return (
     <>
@@ -14,6 +20,7 @@ function App() {
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt={`${t("viteLogo")}`} />
         </a>
+
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img
             src={reactLogo}
@@ -22,16 +29,30 @@ function App() {
           />
         </a>
       </div>
+
       <h1>{t("vitePlusReact")}</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           {t("countIs")} {count}
         </button>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <Trans i18nKey="editAndSave">
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </Trans>
         </p>
       </div>
+
       <p className="read-the-docs">{t("clickToLearnMore")}</p>
+
+      <button type="button" onClick={() => changeLanguage("en")}>
+        en
+      </button>
+
+      <button type="button" onClick={() => changeLanguage("he")}>
+        he
+      </button>
     </>
   );
 }
