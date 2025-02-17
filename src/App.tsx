396acc72545ead5e0b0d907793298b057c41f067
Shay Cojocaru <shayc@outlook.com>
2025-02-17 02:11:37 +0200

    feat(Home): implement Home component with translation and interactive elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { Route, Routes } from "react-router";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
 import { useDocumentLanguage } from "./hooks/useDocumentLanguage";
 import { About } from "./pages/About/About";
 import { Home } from "./pages/Home/Home";
-import viteLogo from "/vite.svg";
 
 export function App() {
-  const [count, setCount] = useState(0);
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
   useDocumentLanguage();
 
@@ -20,36 +16,6 @@ export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt={`${t("viteLogo")}`} />
-        </a>
-
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt={`${t("reactLogo")}`}
-          />
-        </a>
-      </div>
-
-      <h1>{t("vitePlusReact")}</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {t("countIs")} {count}
-        </button>
-
-        <p>
-          <Trans i18nKey="editAndSave">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </Trans>
-        </p>
-      </div>
-
-      <p className="read-the-docs">{t("clickToLearnMore")}</p>
-
       <button type="button" onClick={() => changeLanguage("en")}>
         en
       </button>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,42 @@
+import { useState } from "react";
+import { Trans, useTranslation } from "react-i18next";
+import reactLogo from "../../assets/react.svg";
+import viteLogo from "/vite.svg";
+
 export function Home() {
-  return "Home";
+  const { t } = useTranslation();
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt={`${t("viteLogo")}`} />
+        </a>
+
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt={`${t("reactLogo")}`}
+          />
+        </a>
+      </div>
+
+      <h1>{t("vitePlusReact")}</h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          {t("countIs")} {count}
+        </button>
+
+        <p>
+          <Trans i18nKey="editAndSave">
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </Trans>
+        </p>
+      </div>
+      <p className="read-the-docs">{t("clickToLearnMore")}</p>
+    </div>
+  );
 }
