9eacaf0437fc3d6f8344e00026e83c83b4f5a78e
Shay Cojocaru <shayc@outlook.com>
2025-02-19 19:43:08 +0200

    feat(tests): enhance custom render function with MemoryRouter for routing support

--- src/tests/testUtils.tsx
+++ src/tests/testUtils.tsx
@@ -1,12 +1,15 @@
 import { render } from "@testing-library/react";
 import React, { ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
+import { MemoryRouter } from "react-router";
 import { i18n } from "../i18n/i18n";
 
 const customRender = (ui: React.ReactElement, options = {}) =>
   render(ui, {
     wrapper: ({ children }: { children: ReactNode }) => (
-      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+      <MemoryRouter>
+        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+      </MemoryRouter>
     ),
     ...options,
   });
